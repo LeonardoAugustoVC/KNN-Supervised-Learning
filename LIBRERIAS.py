d36da6c67d9c4e6b3f5ee5f111d91f60bd822c87
LeonardoAugustoVC <135409863+LeonardoAugustoVC@users.noreply.github.com>
2023-09-18 13:49:03 -0600

    Add files via upload

---
+++ LIBRERIAS.py
@@ -0,0 +1,40 @@
+# Librerias Basicas (Basic Libraries)
+
+import pandas as pd
+
+import numpy as np
+
+import math
+
+from math import sqrt
+
+from tabulate import tabulate
+ 
+import seaborn as sns
+
+
+# Librerias para Descripción Estadística (Stactic Descripcion Libraries)
+
+import matplotlib.pyplot as plt
+
+# Algoritmo K-NN  (K-NN Algorithm)
+
+import scipy
+
+import scipy.stats
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import mean_squared_error
+
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.ensemble import BaggingRegressor
+
+# Método KNeighborsClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+
+# Método KNeighborsRegressor
+
+from sklearn.neighbors import KNeighborsRegressor
